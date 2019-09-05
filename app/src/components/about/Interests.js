d3649886dcdced8cfd98adf6d8ab284c8625fd24
La Fountain <github email address>
2019-09-04 21:38:17 -0400

    Added interest and skill data

--- app/src/components/about/Interests.js
+++ app/src/components/about/Interests.js
@@ -22,6 +22,67 @@ class Interests extends Component {
         <Typography style={{ fontSize: "1rem", color: "white" }}>
           My interests are requested..
         </Typography>
+        <div className="interest">
+          <ol className="skills-list">
+            <li className="item-list double">
+              <i className="fab fa-bitcoin" />
+              <h3>Blockstack/DAPP</h3>
+            </li>
+            <li className="item-list">
+              <i className="fas fa-network-wired" />
+              <h3>Systems Engineer</h3>
+            </li>
+            <li className="item-list">
+              <i className="fab fa-aws" />
+              <h3>AWS</h3>
+            </li>
+            <li className="item-list">
+              <i className="fas fa-briefcase" />
+              <h3>Entreprenuer</h3>
+            </li>
+          </ol>
+          <div className="area-interest">
+            <h5>Areas of interests</h5>
+            <ol>
+              <li>Aging out Youth</li>
+              <li>Data Privacy</li>
+              <li>Data Solutions</li>
+              <li>Small Business Development</li>
+              <li>Systems Architecture</li>
+            </ol>
+            <div className="hobby">
+              <ol className="skills-list">
+                <li className="item-list double">
+                  <i className="fas fa-campground" />
+                  <h3>Outdoor Activities</h3>
+                </li>
+                <li className="item-list">
+                  <i className="fas fa-history" />
+                  <h3>Enjoy History</h3>
+                </li>
+                <li className="item-list">
+                  <i className="fas fa-paw" />
+                  <h3>Pet Lover</h3>
+                </li>
+                <li className="item-list">
+                  <i className="fas fa-running" />
+                  <h3>Working Out</h3>
+                </li>
+              </ol>
+              <div className="area-hobby">
+                <h2>My Hobbies</h2>
+                <p>
+                  I like to consider myself very diverse in my hobbies. I am
+                  avivd sports enthusiam, having recieve a full athletic
+                  scholarship, and winning the MSG Golden Gloves Tournament. In
+                  addition, I enjoy being outdoors, hiking,skiing, canoeing and
+                  much more. Strategic board games, reading about history are a
+                  few things that I enjoy.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </Grid>
     );
   }
--- app/src/components/about/Skills.js
+++ app/src/components/about/Skills.js
@@ -22,6 +22,56 @@ class Skills extends Component {
         <Typography style={{ fontSize: "1rem", color: "white" }}>
           More Skills Needed;
         </Typography>
+        <ol className="skills-list">
+          <li className="item-list double">
+            <i className="fab fa-html5" />
+            <h3>HTML</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-css3-alt" />
+            <h3>CSS</h3>
+          </li>
+          <li className="item-list">
+            <i className="fas fa-gem" />
+            <h3>Ruby on Rails</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-js" />
+            <h3>JavaScript</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-java" />
+            <h3>Java</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-react" />
+            <h3>React/Redux</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-node" />
+            <h3>NodeJs</h3>
+          </li>
+          <li className="item-list">
+            <i className="fas fa-database" />
+            <h3>MySQL/Postgresql</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-github" />
+            <h3>GitHub</h3>
+          </li>
+          <li className="item-list">
+            <i className="fas fa-cloud" />
+            <h3>Salesforce</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-hubspot" />
+            <h3>Hubspot</h3>
+          </li>
+          <li className="item-list">
+            <i className="fab fa-adobe" />
+            <h3>Adobe Photoshop</h3>
+          </li>
+        </ol>
       </Grid>
     );
   }
